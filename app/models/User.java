dc1d0bd75a1f143eaa2ed2f97297e10a9f6bef7a
Slade Taylor <sladeoss@gmail.com>
2015-04-20 18:01:10 +0200

    Database Test, Working push to database, ugly as hell code

---
+++ app/models/User.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.validation.*;
+import play.db.ebean.Model;
+
+@Entity
+public class User extends Model{
+
+@Id @Constraints.Required
+public String username;
+
+@Constraints.Required
+public String password;
+
+public static Model.Finder<String, User> find = new Model.Finder<String, User>(String.class, User.class);
+}
